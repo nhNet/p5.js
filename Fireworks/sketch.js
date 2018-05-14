7cc022bce8d353bf2b824342df2c3804478439e9
Browsers <38656592+nhNet@users.noreply.github.com>
2018-05-14 08:48:42 +0200

    Add files via upload

---
+++ Fireworks/sketch.js
@@ -0,0 +1,30 @@
+// Nicholas Huaman
+var fireworks = [];
+var gravity;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  colorMode(HSB);
+  gravity = createVector(0, 0.12);
+  stroke(255);
+  strokeWeight(4);
+  background(0);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(0, 0, 0, 25);
+  
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+  
+  for (var i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+    
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
